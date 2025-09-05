37ff6354a52f9ef3b7207d0bcba6031aad95106c
tanmayAndro <tanmayAndro@gmail.com>
2025-09-05 17:03:45 +0530

    fix code for ethnic composition

--- app/controllers/primary_indicators_controller.rb
+++ app/controllers/primary_indicators_controller.rb
@@ -6,6 +6,68 @@ class PrimaryIndicatorsController < ApplicationController
 	end	
 
 
+	# def fetch_primary_indicator_data_fast_sql
+	# 	department_code        = params[:department_code]
+	# 	muncipality_code       = params[:muncipality_code]
+	# 	unit_info              = params[:class_code]
+	# 	rural_sector_code      = params[:rural_sector_code]
+	# 	rural_section_code     = params[:rural_section_code]
+	# 	populated_center_code  = params[:populated_center_code]
+	# 	urban_sector_code      = params[:urban_sector_code]
+	# 	urban_section_code     = params[:urban_section_code]
+	# 	column_name            = params[:column]
+
+	# 	unless PrimaryIndicator.column_names.include?(column_name)
+	# 		return render json: { error: "Invalid column name" }, status: :bad_request
+	# 	end
+
+	# 	group_field =
+	# 		if urban_section_code.present?
+	# 		:block
+	# 		elsif urban_sector_code.present?
+	# 		:urban_section
+	# 		elsif populated_center_code.present?
+	# 		:urban_sector
+	# 		elsif rural_section_code.present?
+	# 		:populated_center
+	# 		elsif rural_sector_code.present?
+	# 		:rural_section
+	# 		elsif unit_info.present?
+	# 		:rural_sector
+	# 		elsif muncipality_code.present?
+	# 		:unit_info
+	# 		elsif department_code.present?
+	# 		:muncipality_code
+	# 		else
+	# 		:department_code
+	# 		end
+
+	# 	query = PrimaryIndicator
+	# 		.joins("INNER JOIN unique_georreferenciacions 
+	# 				ON unique_georreferenciacions.dane_code_anm = primary_indicators.dane_code")
+
+	# 	query = query.where(unique_georreferenciacions: { department_code: department_code }) if department_code.present?
+	# 	query = query.where(unique_georreferenciacions: { muncipality_code: muncipality_code }) if muncipality_code.present?
+	# 	query = query.where(unique_georreferenciacions: { unit_info: unit_info }) if unit_info.present?
+	# 	query = query.where(unique_georreferenciacions: { rural_sector: rural_sector_code }) if rural_sector_code.present?
+	# 	query = query.where(unique_georreferenciacions: { rural_section: rural_section_code }) if rural_section_code.present?
+	# 	query = query.where(unique_georreferenciacions: { populated_center: populated_center_code }) if populated_center_code.present?
+	# 	query = query.where(unique_georreferenciacions: { urban_sector: urban_sector_code }) if urban_sector_code.present?
+	# 	query = query.where(unique_georreferenciacions: { urban_section: urban_section_code }) if urban_section_code.present?
+
+	# 	results = query.group("unique_georreferenciacions.#{group_field}")
+	# 					.average("primary_indicators.#{column_name}")
+
+	# 	formatted = results.map do |g, avg|
+	# 		{
+	# 		group_field => g,
+	# 		total: avg.nil? ? nil : avg.to_f.round(2)
+	# 		}
+	# 	end
+
+	# 	render json: { results: formatted }
+	# end
+
 	def fetch_primary_indicator_data_fast_sql
 		department_code        = params[:department_code]
 		muncipality_code       = params[:muncipality_code]
@@ -23,28 +85,28 @@ class PrimaryIndicatorsController < ApplicationController
 
 		group_field =
 			if urban_section_code.present?
-			:block
+				:block
 			elsif urban_sector_code.present?
-			:urban_section
+				:urban_section
 			elsif populated_center_code.present?
-			:urban_sector
+				:urban_sector
 			elsif rural_section_code.present?
-			:populated_center
+				:populated_center
 			elsif rural_sector_code.present?
-			:rural_section
+				:rural_section
 			elsif unit_info.present?
-			:rural_sector
+				:rural_sector
 			elsif muncipality_code.present?
-			:unit_info
+				:unit_info
 			elsif department_code.present?
-			:muncipality_code
+				:muncipality_code
 			else
-			:department_code
+				:department_code
 			end
 
 		query = PrimaryIndicator
-			.joins("INNER JOIN unique_georreferenciacions 
-					ON unique_georreferenciacions.dane_code_anm = primary_indicators.dane_code")
+							.joins("INNER JOIN unique_georreferenciacions 
+											ON unique_georreferenciacions.dane_code_anm = primary_indicators.dane_code")
 
 		query = query.where(unique_georreferenciacions: { department_code: department_code }) if department_code.present?
 		query = query.where(unique_georreferenciacions: { muncipality_code: muncipality_code }) if muncipality_code.present?
@@ -55,18 +117,44 @@ class PrimaryIndicatorsController < ApplicationController
 		query = query.where(unique_georreferenciacions: { urban_sector: urban_sector_code }) if urban_sector_code.present?
 		query = query.where(unique_georreferenciacions: { urban_section: urban_section_code }) if urban_section_code.present?
 
-		results = query.group("unique_georreferenciacions.#{group_field}")
-						.average("primary_indicators.#{column_name}")
+		if column_name == "ethnic_composition"
+			# Extract all unique keys safely (ignore NULLs / scalars)
+			keys = PrimaryIndicator.where("jsonb_typeof(ethnic_composition) = 'object'")
+														.distinct
+														.pluck(Arel.sql("jsonb_object_keys(ethnic_composition)"))
+														.compact
+														.uniq
 
-		formatted = results.map do |g, avg|
-			{
-			group_field => g,
-			total: avg.nil? ? nil : avg.to_f.round(2)
-			}
+			results = {}
+			keys.each do |key|
+				sql = Arel.sql("unique_georreferenciacions.#{group_field}, 
+												AVG((primary_indicators.ethnic_composition->>'#{key}')::numeric)")
+				avg_for_key = query.group("unique_georreferenciacions.#{group_field}")
+													.pluck(sql)
+
+				avg_for_key.each do |g, avg|
+					results[g] ||= {}
+					results[g][key] = avg.nil? ? nil : avg.to_f.round(2)
+				end
+			end
+
+			formatted = results.map do |g, values|
+				{ group_field => g }.merge(values)
+			end
+		else
+			results = query.group("unique_georreferenciacions.#{group_field}")
+										.average("primary_indicators.#{column_name}")
+
+			formatted = results.map do |g, avg|
+				{
+					group_field => g,
+					total: avg.nil? ? nil : avg.to_f.round(2)
+				}
+			end
 		end
 
 		render json: { results: formatted }
-	end
+  end
 
 
 
