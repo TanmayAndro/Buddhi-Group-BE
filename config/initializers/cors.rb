5006e29a640a03fe657112592b3ceae2a32d6cf7
tanmayAndro <tanmayAndro@gmail.com>
2025-09-05 21:38:08 +0530

    add vercel url for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
-            "https://census-frontend-ttbh-git-main-jnvdurgas-projects.vercel.app",
+            "https://census-frontend-ttbh.vercel.app", # ✅ Production Vercel domain
+            "https://census-frontend-ttbh-git-main-jnvdurgas-projects.vercel.app", # Preview domain
             "https://buddhi-group-be.onrender.com"
 
     resource "*",
