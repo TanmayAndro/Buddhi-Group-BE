5121c99feac8324ba0a100d12d760271e582007e
tanmayAndro <tanmayAndro@gmail.com>
2025-09-05 21:31:37 +0530

    fix cors issue for BE

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
-            "https://census-frontend-ttbh-git-main-jnvdurgas-projects.vercel.app"
+            "https://census-frontend-ttbh-git-main-jnvdurgas-projects.vercel.app",
+            "https://buddhi-group-be.onrender.com"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
-
