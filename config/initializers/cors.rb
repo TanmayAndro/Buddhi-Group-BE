b42c8050c8293072870749a6f95e589024c7c2fa
tanmayAndro <tanmayAndro@gmail.com>
2025-09-05 21:10:17 +0530

    fix cors issue for live url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173",
+            "https://census-frontend-ttbh-git-main-jnvdurgas-projects.vercel.app"
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
